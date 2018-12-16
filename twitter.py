6355767487b4dfa45bd8a44a0573ae4c5da99d4a
Nipun <2nipun@gmail.com>
2018-12-16 01:25:35 -0800

    first

---
+++ twitter.py
@@ -0,0 +1,6 @@
+#twitter API test python program
+import twitter
+# for i in dir(twitter): print i
+
+
+api = twitter.Api(consumer_key='U94NpXwruZdrgsEqqPRYOxcLp', consumer_secret='nKEUuqMKSErcYwwu4LkxwBKwRX1nsahD0s5CNsVxBzZ2qSKaLZ', access_token_key='19895352-zZgKJddxwnuENnn6mLidavm8QYpx75Dpk4YIZdfqz', access_token_secret='BIX3pgJzMum9Qwf24xlBe1JxBjxkPL1xBCVWdTrXUr0vS')
